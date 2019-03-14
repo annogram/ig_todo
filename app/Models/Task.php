d3e11961f8beb22a4b6380e96cb071c8392183a0
Ilia Galperin <galperinjob@gmail.com>
2019-03-14 21:58:19 +1300

    add user to Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -16,4 +16,12 @@ class Task extends Model
         'done_at'
     ];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
